e1e26f662bef528b3c8de466104611303903cf53
Adriano Di Giovanni <me@adrianodigiovanni.com>
2015-07-09 15:55:36 +0200

    add getSize iOS

--- src/ios/CDVFileXtra.m
+++ src/ios/CDVFileXtra.m
@@ -1,6 +1,7 @@
 #import "CDVFileXtra.h"
 
 @implementation CDVFileXtra
+
 -(void)getFreeDiskSpace:(CDVInvokedUrlCommand *)command {
 
     NSFileManager* fileManager = [[NSFileManager alloc] init];
@@ -19,4 +20,23 @@
 
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
+
+-(void)getSize:(CDVInvokedUrlCommand *)command {
+
+    NSString* path = [command argumentAtIndex:0];
+
+    NSFileManager* fileManager = [[NSFileManager alloc] init];
+    NSError*__autoreleasing error = nil;
+    NSDictionary* dictionary = [fileManager attributesOfItemAtPath:path error:&error];
+
+    CDVPluginResult* result = nil;
+
+    if (error) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageToErrorObject:(int)error.code];
+    } else if (dictionary) {
+        NSNumber* bytes = [dictionary objectForKey:NSFileSize];
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[bytes stringValue]];
+    }
+}
+
 @end
--- www/FileXtra.js
+++ www/FileXtra.js
@@ -12,6 +12,16 @@ var
         };
 
       exec(done, errorCallback, 'FileXtra', 'getFreeDiskSpace', []);
+    },
+    getSize: function (path, successCallback, errorCallback) {
+
+      var
+        done = function (result) {
+          var bytes = parseInt(result, 10);
+          successCallback(bytes);
+        };
+
+      exec(done, errorCallback, 'FileXtra', 'getSize', [ path ]);
     }
   };
 
